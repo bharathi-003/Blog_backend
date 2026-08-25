88070dbfba3d313abd6a520855dcf06236238e01
bharathi-003 <bharathiananth003@gmail.com>
2026-08-25 14:13:01 +0530

    Fix server port logging to use environment variable

--- backend/server.js
+++ backend/server.js
@@ -14,5 +14,5 @@ app.use("/posts", postRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${5000}`);
+  console.log(`Server running on port ${PORT}`);
 });
