60539ed211b65dce148c701fb185348cc8f8c27b
bharathi-003 <bharathiananth003@gmail.com>
2026-08-25 14:11:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ app.use(bodyParser.json());
 
 app.use("/posts", postRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${5000}`);
 });
